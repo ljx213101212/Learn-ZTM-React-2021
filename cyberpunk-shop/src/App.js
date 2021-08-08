abb23e2898bc7c303942fdd654cc9e6129eaec50
ljx213101212 <lijixiang.nus@gmail.com>
2021-08-08 17:26:16 +0800

    add global window event listener module

--- cyberpunk-shop/src/App.js
+++ cyberpunk-shop/src/App.js
@@ -5,8 +5,20 @@ import Home from './pages/home/home';
 import ShopPage from './pages/shop/shop';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up';
 import Header from './components//header/header';
+import {
+  addWindowEventListeners,
+  removeWindowListener,
+} from './utils/windowEventListener';
 
 class App extends Component {
+  componentDidMount() {
+    addWindowEventListeners();
+  }
+
+  componentWillUnmount() {
+    removeWindowListener();
+  }
+
   render() {
     return (
       <div className="App">
